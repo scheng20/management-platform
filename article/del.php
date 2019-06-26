efac45b3b0cd880aa3ff16d670b5c40b366942b4
Sheena Cheng <chengsheena25@gmail.com>
2019-06-26 14:39:27 -0600

    Create del.php

---
+++ article/del.php
@@ -0,0 +1,23 @@
+<?php
+
+    header('Content-Type: application/json');
+    require_once $_SERVER['DOCUMENT_ROOT'] .'/source/config.php';
+    
+    class C_result 
+    {
+        public $error_code=0;
+        public $error_msg='SUCCESS';
+    }
+    
+    $sql = 'DELETE FROM article WHERE id = '.$_GET['currentId'];
+    
+    if ($sqlresult = $mysqli->query($sql))
+    {
+        $res = new C_result;
+        $jsonresult=json_encode($res, JSON_UNESCAPED_UNICODE);
+        echo urldecode($jsonresult);
+    }
+    
+    $mysqli->close();
+    
+?>
